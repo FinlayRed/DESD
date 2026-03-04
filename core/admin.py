84f6ae1fdab12af7fa12d2ebdee3bc277d467e43
Finlay <finlayredding+github@gmail.com>
2026-03-04 16:36:31 +0000

    Register core models in Django admin

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,43 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Category, Order, OrderItem, Payment, Producer, Product
+
+
+@admin.register(Producer)
+class ProducerAdmin(admin.ModelAdmin):
+    list_display = ("business_name", "user", "organic", "postcode")
+    list_filter = ("organic",)
+    search_fields = ("business_name", "user__username", "postcode")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name", "description")
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "price", "is_active", "producer", "updated_at")
     list_filter = ("category", "is_active")
-    search_fields = ("name", "description", "producer__username")
+    search_fields = ("name", "description", "producer__user__username")
 
-    def producer_username(self, obj):
-        return obj.producer.user.username
-    producer_username.short_description = "Producer username"
 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "paid", "created_at", "updated_at")
+    list_filter = ("paid", "created_at")
+    search_fields = ("customer__username",)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "product", "quantity", "price")
+    list_filter = ("product",)
+    search_fields = ("order__id", "product__name")
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "order", "total_amount", "network_commission", "producer_amount", "created_at")
+    search_fields = ("order__id",)
